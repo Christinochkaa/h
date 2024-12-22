1ef10fcc65557654c38328c770d615b2b4c2dbaa
Christinochkaa <146668828+Christinochkaa@users.noreply.github.com>
2024-12-23 01:27:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] sequence = { 1, 2, 3, 4, 5, 100, 50, 25, 75, 10, 90, 80, 70, 60, 55, 45, 40, 30, 20, 15 };
+
+        int maxSumOfSquares = 0;
+
+        int length = sequence.Length;
+
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = i + 10; j < length; j++)
+            {
+                int sumOfSquares = sequence[i] * sequence[i] + sequence[j] * sequence[j];
+
+                if (sumOfSquares > maxSumOfSquares)
+                {
+                    maxSumOfSquares = sumOfSquares;
+                }
+            }
+        }
+
+        Console.WriteLine("Максимальная сумма квадратов: " + maxSumOfSquares);
+    }
+}
